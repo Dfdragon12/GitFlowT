b6678dec22421b92cc40164b915d7c8a19a8ab1b
Dfdragon12 <61027677+Dfdragon12@users.noreply.github.com>
2022-08-29 18:57:27 -0500

    Operación de multiplicar. DF

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import multiplicar.Multiplicar;
+
+public class Main {
+    public static void main(String[] args) {
+        Multiplicar multi = new Multiplicar();
+        multi.operacion();
+    }
+}
