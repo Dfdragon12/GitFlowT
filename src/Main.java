eafea580c62291ae76ceb93391c70211f3f312b4
matmize <mateomontoya5@gmail.com>
2022-08-30 07:04:16 -0500

    funcion resta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
 
+import resta.Resta;
+public class Main {
+
+    public static void main(String[] args) {
+    Resta resta = new Resta();
+    System.out.println(resta.resta());
     }
 }
--- src/resta/Resta.java
+++ src/resta/Resta.java
@@ -1,4 +1,10 @@
 package resta;
 
 public class Resta {
+    public int resta(){
+    int num1 = 500;
+    int num2 = 224;
+    int resta = num1-num2;
+    return resta;
+    }
 }
