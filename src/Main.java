5dd3926cb61ec9b2c1d636bce8ca4388be169b75
julianCuellarP <juliancuellarp@gmail.com>
2022-08-29 19:22:28 -0500

    funcion suma

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import suma.Suma;
+public class Main {
+
+    public static void main(String[] args) {
+       Suma suma = new Suma();
+       suma.sumatoria();
+
+    }
 }
--- src/suma/Suma.java
+++ src/suma/Suma.java
@@ -1,2 +1,19 @@
-package suma;public class Suma {
+package suma;
+
+import java.util.Scanner;
+
+public class Suma {
+    public void sumatoria (){
+       int a,b;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("ingrese digito 1");
+        a = scan.nextInt();
+
+        System.out.println("ingrese digito 2");
+        b = scan.nextInt();
+
+        System.out.println(a+b);
+    }
 }
